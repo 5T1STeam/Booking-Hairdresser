de54921d5ed965853ba9d36fbde3d95d1f96deba
TieensHaf <tienha.31191026680@st.ueh.edu.vn>
2021-10-30 10:02:36 +0700

    layout profile

--- mvc/models/detailShopModel.php
+++ mvc/models/detailShopModel.php
@@ -5,6 +5,137 @@ class detailShopModel extends db{
         $qr = "SELECT * FROM tbl_shopbanner WHERE ShopId=".$id."";
         return mysqli_query($this->con,$qr);
     }
+    public function GetNameShop($id){
+        $qr = "SELECT * FROM tbl_user WHERE Id=".$id."";
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetServiceShop($id){
+        $qr = "SELECT * FROM tbl_shopservices WHERE ShopId=".$id."";
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetService(){
+        $qr = "SELECT * FROM tbl_services";
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetRule($id){
+        $qr = "SELECT * FROM tbl_shoprules WHERE ShopId=". $id. "" ;
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetFiveImage($id){
+        $qr = "SELECT  * FROM tbl_feedbacks WHERE ShopId= $id  "  ;
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetFeedBack($id){
+        $qr = "SELECT * FROM tbl_feedbacks WHERE ShopId=".$id."";
+        $querry= mysqli_query($this->con,$qr);
+        $result =array();
+        $sumR=0;
+        $z=0;
+        $x=0;
+        $c=0;
+        $v=0;
+        $b=0;
+        while($rows=mysqli_fetch_array($querry))
+        {
+            array_push($result,$rows);
+            $sumR+=$rows['Rating'];
+
+            switch($rows['Rating']){
+                case 5: {
+                    $z++;
+                    break;
+                }
+                case 4: {
+                    $x++;
+                    break;
+                }
+                case 3:{
+                    $c ++;
+                    break;
+                }
+                case 2:{
+                    $v++;
+                }
+                case 1:{
+                    $b++;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+       
+        $ts= round($sumR / count($result),1);
+        $result['AverageRating']=$ts;
+        $result['QuantityRating']=$z+$x+$c+$v+$b;
+        $result['FiveStarRating']=$z;
+        $result['FourStarRating']=$x;
+        $result['ThreeStarRating']=$c;
+        $result['TwoStarRating']=$v;
+        $result['OneStarRating']=$b;
+        return $result;
+    }
+    public function GetServiceById($id){
+        $qr = "SELECT * FROM tbl_services WHERE Id=$id";
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetServiceFeedback($id){
+        $qr = "SELECT * FROM tbl_feedbacks WHERE ShopId=".$id."";
+        $retreat= mysqli_query($this->con,$qr);
+        $result=array();
+        while($rows= mysqli_fetch_array($retreat)){
+            array_push($result,$rows);
+        }
+        return $result;
+    }
+    public function GetNameUser($id){
+        $qr ="SELECT * FROM tbl_user WHERE Id=$id";
+        $querry= mysqli_query($this->con,$qr);
+        $item= mysqli_fetch_array($querry);
+            $a= $item["AddressPath"];
+            $qa= "SELECT * FROM  tbl_addresspath where Id=$a";
+            $querry1= mysqli_query($this->con,$qa);
+            $row1=mysqli_fetch_array($querry1);
+            $item['Ward']= $row1['Name'];
+            $a= $row1['ParentPathId'];
+            $qs= "SELECT * FROM  tbl_addresspath where Id=$a";
+            $querry2=mysqli_query($this->con,$qs);
+            $row2=mysqli_fetch_array($querry2);
+            $item['District'] =$row2['Name'];
+            $a =$row2['ParentPathId'];
+            $qc= "SELECT * FROM  tbl_addresspath where Id=$a";
+            $querry3=mysqli_query($this->con,$qc);
+            $row3=mysqli_fetch_array($querry3);
+            $item['Province']= $row3['Name'];
+       
+        return $item;
+
+       
+
+    }
+    public function GetStaff($id){
+        $qr = "SELECT * FROM tbl_staff WHERE ShopId=$id";
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetCalendar($id){
+        $qr = "SELECT * FROM tbl_calendar WHERE ShopId=$id";
+        $querry= mysqli_query($this->con,$qr);
+        $result =array();
+        while($row=mysqli_fetch_array($querry)){
+            array_push($result,$row);
+        }
+        return $result;
+    }
+    public function GetDayOfWEEK($id){
+        $qr = "SELECT * FROM tbl_dayofweek WHERE Id=$id";
+        return mysqli_query($this->con,$qr);
+    }
+    public function GetLikeDisLike($id){
+        $qr = "SELECT * FROM tbl_likefeedback WHERE FeedbackId=$id";
+        return mysqli_query($this->con,$qr); 
+    }
+   
     
+
 }
 ?>
---
+++ mvc/views/profile.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chi tiết</title>
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/style-sevice-page.css">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href=".../../../Booking-Hairdresser/css/popup.css">
+  <link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/userprofileStyle/style-profile-page.css">
+  <link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/userprofileStyle/style-favorite-page.css">
+  <link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/userprofileStyle/style-infor-page.css">
+  <link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/userprofileStyle/style-favorite-page.css">
+</head>
+<div class="row profile-page">
+  <div class=" col-lg-4 col-xl-3  col-md-12 ">
+    <div class="row avatar-profile"><span class="avt"> <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 18 18">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+        </svg>
+        Profile </span>
+    </div>
+    <a class="sss" href="./pages/thongtintaikhoan.php">
+      <div class=" row list-profile infor-account"><span class="list"> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 18 18">
+            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+          </svg> Thông tin tài khoản</span>
+      </div>
+    </a>
+    <a class="sss" href="../userprofile/notification.html">
+      <div class=" row list-profile  "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="30" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 18 18">
+            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+          </svg>Thông báo của tôi<div class="icon-noti text-center tex">4</div></span></div>
+    </a>
+    <a class="sss" href="./pages/cuahangyeuthich.php">
+      <div class=" row list-profile fvr-shop "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="30" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 18 18">
+            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+          </svg> Cửa hàng yêu thích</span> </div>
+    </a>
+    <a class="sss" href="../userprofile/schedule.html">
+      <div class=" row list-profile lich "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-calendar2-day-fill" viewBox="0 0 19 20">
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zm9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5zm-2.24 4.855a.428.428 0 1 0 0-.855.426.426 0 0 0-.429.43c0 .238.192.425.43.425zm.337.563h-.672v4.105h.672V8.418zm-6.867 4.105v-2.3h2.261v-.61H4.684V7.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V9.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105z" />
+          </svg> Lịch hẹn</span></div>
+    </a>
+    <a class="sss" href="../userprofile/reviews.html">
+      <div class=" row list-profile "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="30" fill="currentColor" class="bi bi-star-half" viewBox="0 0 18 20">
+            <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
+          </svg> Đánh giá của tôi</span></div>
+    </a>
+    <a class="sss" href="../userprofile/history.html">
+      <div class=" row list-profile "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 18 18">
+            <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z" />
+            <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z" />
+            <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z" />
+          </svg> Lịch sử booking</span></div>
+    </a>
+    <a class="sss" href="../userprofile/Voucher.html">
+      <div class=" row list-profile "><span class="list"><img class="icon-infor" src="../../../Booking-Hairdresser/public/icon/magiamgia.png" alt=""> Mã giảm giá</div>
+    </a>
+    <a class="sss" href="../userprofile/Member.html">
+      <div class=" row list-profile "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-square" viewBox="0 0 18 18">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
+          </svg> Hội Viên </span> </div>
+    </a>
+    <a class="sss" href="../userprofile/polici.html">
+      <div class=" row list-profile "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-shield-lock-fill" viewBox="0 0 18 18 ">
+            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z" />
+          </svg> Chính sách bảo mật</span></div>
+    </a>
+    <a class="sss" href="../userprofile/TermOfService.html">
+      <div class=" row list-profile "><span class="list"><img class="icon-infor" src="../../../Booking-Hairdresser/public/icon/dkdv.png" alt=""> Điều khoản dịch vụ</span> </div>
+    </a>
+    <a class="sss" href="../login.html">
+      <div class=" row list-profile "><span class="list"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 18 18">
+            <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+            <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+          </svg> Đăng xuất</span></div>
+    </a>
+
+
+
+
+  </div>
+  <div class="col-lg-8 col-xl-9 col-md-12">
+  <?php
+        require_once "./mvc/views/pages/".$data["page"].".php"
+  ?>
+  </div>
+</div>
+
+</html>
