251d42096c6708a0be6680b99619cdbe8a616eb5
TieensHaf <tienha.31191026680@st.ueh.edu.vn>
2021-11-08 17:15:37 +0700

    them magiamgiaphp

---
+++ mvc/views/pages/magiamgia.php
@@ -0,0 +1,133 @@
+<link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/userprofileStyle/VoucherStyle.css">
+<link rel="stylesheet" href="../../../Booking-Hairdresser/public/css/userprofileStyle/memberstyle.css">
+<div class="container vouchers">
+                    <h3 class="voucherHeader"> Mã giảm giá</h3>
+                    
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-12 boxrating">
+                                    
+                                    <div class="chitietbox ">
+                                        <div class="row  pt-2 pr-2 pl-2 ">
+                                            <div class="col-md-6 col-xs-12"><h5>Chi tiêu</h5></div>
+                                            <div class="col-md-6 col-xs-12"><button class="passbtn" disabled>Hạng Bạc</button></div>
+                                        </div>
+                                        <div class="pricePass">300.000đ/500.000đ</div>
+                                        <div class="">Chi tiêu thêm 200.000đ để đạt hạng vàng</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-7 col-xs-12">
+                                    <div class="voucherMember">Ưu đãi ranking</div>
+                                    <div>Discount 10% tổng giá trị đơn hàng khi đạt hạng đồng</div>
+                                    <div>Discount 20% tổng giá trị đơn hàng khi đạt hạng bạc</div>
+                                    <div>Discount 30% tổng giá trị đơn hàng khi đạt hạng vàng</div>
+                                    <div>Discount 40% tổng giá trị đơn hàng khi đạt hạng kim cương</div>
+                                </div>                              
+                            </div>
+                            <!-- <div class="row">
+                                <div class="col-lg-4 col-sm-12 boxrating">
+                                    
+                                    <div class="chitietbox ">
+                                        <div class="row  pt-2 pr-2 pl-2 ">
+                                            <div class="col-md-6 col-xs-12"><h5>Chi tiêu</h5></div>
+                                            <div class="col-md-6 col-xs-12"><button class="silverbtn" disabled>Hạng Bạc</button></div>
+                                        </div>
+                                        <div class="pricePass">300.000đ/500.000đ</div>
+                                        <div class="">Chi tiêu thêm 200.000đ để đạt hạng vàng</div>
+                                    </div>
+                                </div>
+                            </div> -->
+                    <div class="allVoucher">
+                        <span class="alls">Tất cả</span>
+                        <div class="row">
+                            <div class="col-md-5">
+                            <div class="col-md-4 col-sm-12 mt-3 ">
+                        <div class="row proColSilver">
+                            <div class="col-3 ">
+                                <img class="img-pro img-fluid" src="img/2.png" alt="">
+                                <div class="vl"></div>
+                                <p class="mt-2 proPercent">Giảm 10%</p>
+
+                            </div>
+                            <div class="col-8">
+                                <h3>Tuấn barber</h3>
+                                <p>Giảm tối đa 10k cho tổng dịch vụ</p>
+                                <p class="proPercent">HSD:20/10/2021</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12 mt-3 ">
+                        <div class="row proColSilver">
+                            <div class="col-3 ">
+                                <img class="img-pro img-fluid" src="img/2.png" alt="">
+                                <div class="vl"></div>
+                                <p class="mt-2 proPercent">Giảm 10%</p>
+
+                            </div>
+                            <div class="col-8">
+                                <h3>Tuấn barber</h3>
+                                <p>Giảm tối đa 10k cho tổng dịch vụ</p>
+                                <p class="proPercent">HSD:20/10/2021</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12 mt-3 ">
+                        <div class="row proColSilver">
+                            <div class="col-3 ">
+                                <img class="img-pro img-fluid" src="img/2.png" alt="">
+                                <div class="vl"></div>
+                                <p class="mt-2 proPercent">Giảm 10%</p>
+
+                            </div>
+                            <div class="col-8">
+                                <h3>Tuấn barber</h3>
+                                <p>Giảm tối đa 10k cho tổng dịch vụ</p>
+                                <p class="proPercent">HSD:20/10/2021</p>
+                            </div>
+                        </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="voucherCode d-flex mb-3 mt-3">
+                                    <div class="row">
+                                        <div class="col-5 mt-1 ml-1 d-inline">
+                                            <img src="../../source/img/Ellipse 3-3.png" alt="">
+                                            <h5 class="nameShopsss">Tuấn Barber</h5>
+                                        </div>
+                                        <div class="col-6 detailzz">
+                                            <div class="detailVoucher">
+                                                <span>Giảm 10k cho tổng giá trị dịch vụ</span>
+                                                <span>HSD:20/10/2021</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="voucherCode d-flex mb-3 mt-3">
+                                    <div class="row">
+                                        <div class="col-5 mt-1 ml-1 d-inline">
+                                            <img src="../../source/img/Ellipse 3-3.png" alt="">
+                                            <h5 class="nameShopsss">Tuấn Barber</h5>
+                                        </div>
+                                        <div class="col-6 detailzz">
+                                            <div class="detailVoucher">
+                                                <span>Giảm 10k cho tổng giá trị dịch vụ</span>
+                                                <span>HSD:20/10/2021</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="voucherCode d-flex mb-3 mt-3">
+                                    <div class="row">
+                                        <div class="col-5 mt-1 ml-1 d-inline">
+                                            <img src="../../source/img/Ellipse 3-3.png" alt="">
+                                            <h5 class="nameShopsss">Tuấn Barber</h5>
+                                        </div>
+                                        <div class="col-6 detailzz">
+                                            <div class="detailVoucher">
+                                                <span>Giảm 10k cho tổng giá trị dịch vụ</span>
+                                                <span>HSD:20/10/2021</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
