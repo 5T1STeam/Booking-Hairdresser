7c207856378aec3af16c25ad752e00291d08a1ef
Tân Larva <duytan01626456764@gmail.com>
2021-11-29 09:31:28 +0700

    fix 29/11

--- public/js/detailShop.js
+++ public/js/detailShop.js
@@ -48,5 +48,14 @@ $(document).ready(function() {
         }
 
     })
+    window.onscroll = function() { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("Menu").style.top = "0";
+        } else {
+            document.getElementById("Menu").style.top = "-150px";
+        }
+    }
 
 })
