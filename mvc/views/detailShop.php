fab53612e7604f2b460e47d3a9c851f5fb414da0
AnhTuanLam <tuanlam.31191026900@st.ueh.edu.vn>
2021-12-04 23:50:46 +0700

    fix iframe link

--- mvc/views/detailShop.php
+++ mvc/views/detailShop.php
@@ -84,7 +84,7 @@
 
                     <div class="avata">
                     <img src="<?php if ($data['GN']['Avatar'] !== null) {
-                            echo  'data:image/jpeg;base64,' . base64_encode($data['GN']['Avatar']);
+                            echo  $data['GN']['Avatar'];
                         } else echo 'https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg' ?>" class='rounded-circle'/>
 
                         
@@ -690,7 +690,7 @@
                         <div class="location">
                             <?php
                             echo "
-                            <iframe width='100%' height='300' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' id='gmap_canvas' src='https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=".$data['GL']."+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'></iframe> 
+                            <iframe width='100%' height='300' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' id='gmap_canvas' src='https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=".$data['GL']."&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'></iframe> 
                                 "; ?>
                            
                             <!-- <div class="GPS">
