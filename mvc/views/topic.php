433a0a13a6acdebfa253f375d5ea273e7f65f741
AnhTuanLam <tuanlam.31191026900@st.ueh.edu.vn>
2021-12-05 00:27:38 +0700

    fix some stupid bug

--- mvc/views/topic.php
+++ mvc/views/topic.php
@@ -33,7 +33,14 @@
             width: 400px;
             height: 400px;
         }
-
+        @media screen and (max-width: 600px) {
+            .img_Top {
+                width: 300px;
+                height: 300px;
+            }
+        }
+            
+       
         .thumbnail {
             width: 100%;
             height: 300px;
@@ -126,11 +133,6 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-        <script src="<?php echo BASE_URL ?>/public/js/owl.carousel.min.js"></script>
-        <script src="<?php echo BASE_URL ?>/public/js/Detail2.script.js"></script>
-        <script src="<?php echo BASE_URL ?>/public/js/nav-sevice-page.js">
-        </script>
-        <script src="<?php echo BASE_URL ?>/public/js/popup.js"></script>
 </body>
 
 </html>
